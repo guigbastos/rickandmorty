d0db01a78d7dfce67b2c60f42c404bbadee77603
guigbastos <guibastos05@live.com>
2026-04-09 19:02:35 -0300

    created dockerfile, added cors config and constants.py

--- app.py
+++ app.py
@@ -3,12 +3,26 @@ from flask_cors import CORS
 from src.models import db, ma
 from config.settings import DATABASE_URI
 from src.routes.character_routes import character_bp
+from src.utils.constants import Environments
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+front_end_url = os.getenv("FRONT_END_URL")
+environment = os.getenv("ENVIRONMENT")
+
+origins_map = {
+    Environments.LOCAL.value: ["*"],
+    Environments.PRODUCTION.value: [front_end_url]
+}
+
+allowed_origin = origins_map.get(environment, origins_map[Environments.PRODUCTION.value])
+
+CORS(app, origins=allowed_origin)
+# resources={r"/*": {"origins": "http://localhost:5173"}}
+
 db.init_app(app)
 ma.init_app(app)
 
